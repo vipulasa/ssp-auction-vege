953e4aea13bbbac15d5f1fff2cddac18eb25760e
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2023-12-14 13:24:42 +0530

    Initial submission

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello', function () {
+    return view('hello', [
+        'name' => 'Taylor',
+        'age' => 30,
+        'address' => 'Hà Nội'
+    ]);
+});
