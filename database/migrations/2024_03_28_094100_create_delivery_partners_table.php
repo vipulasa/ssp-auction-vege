8fd4c807b969ad6a2bc3b53dcaf907310b42e7e3
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2024-03-28 09:49:19 +0530

    Add DeliveryPartner model and update Orders table
    Introduced a new DeliveryPartner model with its corresponding migrations. Updated 'orders' table by adding a 'delivery_partner_id' foreign key. The new structure introduces a relation between orders and delivery partners, enhancing the application's capacity to handle deliveries.

---
+++ database/migrations/2024_03_28_094100_create_delivery_partners_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('delivery_partners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->boolean('status')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('delivery_partners');
+    }
+};
---
+++ database/migrations/2024_03_28_094414_add_delivery_partner_id_to_orders_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('delivery_partner_id')->nullable()->constrained();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
